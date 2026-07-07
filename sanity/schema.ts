d00a48e4d302414997af0e03a656910b3f11576b
AryanUbale7 <aryanubale318@gmail.com>
2026-07-07 20:47:31 +0530

    Fix Sanity schema import paths

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,9 +1,9 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import { author } from './author'
-import { post } from './post'
-import { category } from './category'
-import { resource } from './resource'
-import { faq } from './faq'
+import { author } from './schemas/author'
+import { post } from './schemas/post'
+import { category } from './schemas/category'
+import { resource } from './schemas/resource'
+import { faq } from './schemas/faq'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [author, post, category, resource, faq],
