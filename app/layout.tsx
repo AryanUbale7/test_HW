ee4a491248eb70d7cefb5caf03e121e214b09815
AryanUbale7 <aryanubale318@gmail.com>
2026-07-27 18:46:37 +0530

    Integrate Microsoft Clarity tracking tag xt0kt4huj8 in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,16 @@ export default function RootLayout({
             gtag('config', 'G-6XWY93QSGB');
           `}
         </Script>
+        {/* Microsoft Clarity Tag */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xt0kt4huj8");
+          `}
+        </Script>
       </head>
       <body
         className={`${merriweather.variable} ${inter.variable} font-sans antialiased`}
